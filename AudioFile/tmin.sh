c14f23470ea3f5f10b89f9e818627a79315dd190
Finsenty54 <fin12138@gmail.com>
2021-05-07 15:49:28 +0800

    3.0 i think i find it

--- AudioFile/tmin.sh
+++ AudioFile/tmin.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mkdir in_tmin
-cd in_cmin
+mkdir crash_tmin
+cd out/default/crashes
 for i in *; do
-  afl-tmin -i "$i" -o "../in_tmin/$i" -m none -- ../first_try -d @@
+  afl-tmin -i "$i" -o "../../../crash_tmin/$i" -m none -- ../../../first_try @@
 done
